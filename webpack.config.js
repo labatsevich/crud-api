45603e49e932bcfa2745f1695fea60a73638b66c
yurilabatsevich <yuralabatsevich@gmail.com>
2023-06-28 11:16:15 +0300

    init: start task

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+import path from 'path';
+
+module.exports = {
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
