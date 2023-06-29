84d6b1837cbe07a2ae4dc3455e1aefd3871a1db4
yurilabatsevich <yuralabatsevich@gmail.com>
2023-06-29 14:42:17 +0300

    refactor: refactor entry point file

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
-import http, { IncomingMessage, ServerResponse } from 'http';
+import {createServer} from 'http';
 import dotenv from 'dotenv';
+import { dispatch } from './utils/router';
 
 dotenv.config();
-
 const PORT = Number(process.env.PORT) || 4000;
 
-const server = http.createServer((req:IncomingMessage,res:ServerResponse)=>{
-    res.setHeader('Content-type','text/html');
-    res.write(`<div style="color:red">server started on ${PORT}</div>`);
-    res.end()
-})
+const server = createServer(dispatch);
 
 server.listen(PORT, 'localhost',()=>{
     console.log(`server started on ${PORT}`)
