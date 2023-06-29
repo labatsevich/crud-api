526ad15b0662bab38f5acac474b2b7479e561462
yurilabatsevich <yuralabatsevich@gmail.com>
2023-06-29 14:36:35 +0300

    refactor: refactor user controller

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,16 +1,27 @@
+import { HTTPSTATUS, User } from "../types";
+import { UserModel } from "../models/users";
+import { ServerResponse } from "http";
 
-export const Userscontroller = {
+export const UserController = {
 
-     async getAll(): Promise<unknown[]> {
-        return [
-            {},
-            {}];
-    },
+  async getAll(res:ServerResponse): Promise<void> {
+
+    const entries = await UserModel.getAll();
+    res.statusCode = HTTPSTATUS.Success;
+    res.write(JSON.stringify(entries));
     
-      async getUser(id:number): Promise<unknown> {
-        
-        return {}
-    }
+  },
+
+  async getUser(id: string): Promise<User | unknown> {
+
+    const user = await UserModel.findOrFail(id);
+    return user;
+
+  },
+
+  async create(user: User): Promise<User | undefined> {
+    return UserModel.add(user);
+  }
 
 }
 
