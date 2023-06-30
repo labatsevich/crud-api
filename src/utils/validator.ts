a1e77f359613531f8bba6f4d3068c836885f635c
yurilabatsevich <yuralabatsevich@gmail.com>
2023-06-30 16:15:49 +0300

    feat: add new methods to check  data

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -1,17 +1,36 @@
 import {validate} from 'uuid';
-import { User } from '../types';
+import { IUser } from '../types';
 
 export const Validator = {
-    
-    checkId(id:string):boolean{
-        return validate(id);
+
+    checkRequired(user:IUser){
+        return (('username' in user) && ('age' in user) && ('hobbies' in user));
     },
 
-    checkUser(user:User):boolean{
-    
+    checkAge(user:IUser): boolean {
+        if(typeof user.age !== 'number') return false;
         return true;
-    }
+    },
+    
+    checkName(user:IUser):boolean{
+        if(typeof user.username !== 'string') return false;
+        return true;
+    },
 
+    checkHobbies(user:IUser):boolean {
+        if(!Array.isArray(user.hobbies) || user.hobbies.length === 0 || user.hobbies.some((h) => typeof h !=='string')) return false;
+        return true;
+    },
 
+    checkUser(user:IUser):boolean {
+        
+        if(!this.checkRequired(user)) return false;
 
+        return this.checkAge(user) && this.checkName(user) && this.checkHobbies(user);
+        
+    },
+
+    checkUUID(id:string):boolean{
+        return validate(id);
+    },
 }
